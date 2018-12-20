75b530f8a356b9beee351f3b722e8f5493b03184
MiloGrabie <38583476+MiloGrabie@users.noreply.github.com>
2018-12-20 09:19:40 +0100

    Add files via upload

---
+++ Arduino/main_v2/main/motorPignon.cpp
@@ -0,0 +1,97 @@
+#include "conf.h"
+#include "arduino.h"
+#include "motorPignon.h"
+static boolean etatfindecourseUp = false;
+static boolean etatfindecourseDown = false;
+
+
+void initCapteursfindecourse() { // appelee dans initMotorPignon()
+  pinMode(buttonpin1, INPUT); // declare buttonpin (digital 48) as input (capteur1)
+  pinMode(buttonpin2, INPUT); // declare buttonpin (digital 47) as input (capteur2)
+  //retrouver le code pour les capteurs fin de course et la notice sur : https://www.lextronic.fr/io-compatible-arduino/31650-module-interrupteur-fin-de-course-2-pcs-5410329680794.html
+}
+int valuefindecourseUp;
+int valuefindecourseDown;
+
+boolean isPignonUp() {
+  return (etatfindecourseUp);
+}
+boolean isPignonDown() {
+  return (etatfindecourseDown);
+}
+
+void findecourseUp() {
+  valuefindecourseUp = digitalRead(buttonpin1); // read the value of buttonpin (digital 48)
+  if (valuefindecourseUp == HIGH)
+  { (etatfindecourseUp = true);
+  }
+}
+
+void findecourseDown() {
+  valuefindecourseDown = digitalRead(buttonpin2); // read the value of buttonpin (digital 47)
+  if (valuefindecourseDown == HIGH)
+  { (etatfindecourseDown = true);
+  }
+}
+
+void initMotorPignon() {
+  pinMode(motorPignonUp, OUTPUT);
+  pinMode(motorPignonDown, OUTPUT);
+  initCapteursfindecourse();
+}
+
+char pignonDown ()  {
+  etatfindecourseUp = false;
+  if (!etatfindecourseDown) {
+    digitalWrite (motorPignonDown, HIGH);
+    digitalWrite (motorPignonUp, LOW);
+    do {
+      findecourseDown();
+    } 
+    while (!etatfindecourseDown);
+    pignonStop ();
+    return (PI_OK);
+  } else return (PI_ERR_PIGNON_DEJA_EN_BAS);
+}
+
+void pignonUpBrut(){
+  digitalWrite (motorPignonUp, HIGH);
+  digitalWrite (motorPignonDown, LOW) ;
+}
+
+char pignonUp (){
+  findecourseDown();
+  findecourseUp();
+  Serial.println(digitalRead(buttonpin1));
+  Serial.println(digitalRead(buttonpin2));
+  Serial.println("-------");
+  Serial.println(valuefindecourseUp);
+  Serial.println(valuefindecourseDown);
+  if (!etatfindecourseUp) {
+    digitalWrite (motorPignonUp, HIGH);
+    digitalWrite (motorPignonDown, LOW) ;
+    int i=0;
+    while (!etatfindecourseUp){
+      if(i >= 16){
+        break;
+      }
+      delay(500);
+      findecourseUp();
+      i++;
+    }
+    return (PI_OK);
+  }
+    else{
+      return (PI_ERR_PIGNON_DEJA_EN_HAUT);
+    }
+}
+
+void pignonStop ()  {
+  digitalWrite (motorPignonUp, LOW);
+  digitalWrite (motorPignonDown, LOW);
+}
+
+
+
+
+
---
+++ Arduino/main_v2/main/motorPignon.h
@@ -0,0 +1,10 @@
+void initMotorPignon();
+#define PI_OK 10
+#define PI_ERR_PIGNON_DEJA_EN_HAUT 11
+#define PI_ERR_PIGNON_DEJA_EN_BAS 12
+
+char pignonDown();
+char pignonUp();
+void pignonControl();
+void pignonStop ();
+void pignonUpBrut();
